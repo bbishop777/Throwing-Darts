b5450d44a2c6745cc134db9f0e9bb69cb8680b74
BBishop <brad.bishop68@gmail.com>
2016-01-02 10:08:35 -1000

    Writes script to pass first test and writes 2nd test

---
+++ public/js/throwing-darts.js
@@ -0,0 +1,21 @@
+var dartScoreModule=(function(){
+
+  function dartScore(throwOne, throwTwo, throwThree){
+
+
+  }
+  //window.onload = function() {
+    // var button = document.getElementById('searchbutton').addEventListener('click', function(){
+    //   // Get user input from DOM
+    //   var text1 = document.getElementById('imageType1').value;
+    //   var text2 = document.getElementById('imageType1').value;
+    //   var text3 = document.getElementById('imageType1').value;
+
+    //   scoreArray=[text1, text2, text3];
+    //   console.log(scoreArray);
+    // });
+ // };
+ return dartScore;
+})();
+module.exports=dartScoreModule;
+
--- test/Throwing-Darts_spec.js
+++ test/Throwing-Darts_spec.js
@@ -5,9 +5,17 @@ var expect=chai.expect;
 var dartScoreModule=require('./../public/js/throwing-darts.js');
 
 
-describe('dartScore', function() {
+describe('dartScoreModule', function() {
   it('should exist', function() {
     expect(dartScoreModule).to.exist;
     expect(dartScoreModule).to.be.a('function');
+  });
+
+  it('should return a total score if given 3 scores from dart throws', function(){
+    var result=dartScoreModule([1, 6, 11]);
+
+    expect(result).to.be.a('number');
+    expect(result).to.equal(15);
+
   });
 });
